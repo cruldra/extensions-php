670212e45681c2018b36ecb7237da58ad8855d90
cruldra <cruldra@gmail.com>
2023-08-29 19:20:45 +0800

    + 字符串新增replaceBefore方法

---
+++ readme.md
@@ -0,0 +1,5 @@
+# 初衷
+作为一个``kotlin``爱好者,但被迫用``php``来开发一个项目,所以有了这个库,目的就是为了能在``php``中尽量像在``kotlin``那样写代码
+
+# 安装
+```composer require cruldra/extensions-php```
--- src/ExtArray.php
+++ src/ExtArray.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace ExtensionsPhp;
-
+/**
+ * @template T
+ */
 class ExtArray
 {
-private array $arr;
+    /**
+     * @var T[]
+     */
+    private array $arr;
 
     /**
      * ExtArray constructor.
@@ -16,5 +21,55 @@ private array $arr;
     }
 
 
+    /**
+     * 遍历该数组,将数组中的每一个元素作为参数传入``$callback``中并执行
+     * @param callable(T, mixed):void $callback 回调函数,该函数接受两个参数,第一个参数为数组中的元素,第二个参数为该元素的键名
+     * @return static 返回当前对象
+     */
+    public function forEach(callable $callback): static
+    {
+        foreach ($this->arr as $key => $value) {
+            $callback($value, $key);
+        }
+        return $this;
+    }
+
+    /**
+     * 返回数组中所有的键
+     * @return ExtArray  返回一个包含原数组所有键的新数组
+     */
+    public function keys(): ExtArray
+    {
+        return new ExtArray(array_keys($this->arr));
+    }
+
+    /**
+     * 返回数组中所有的值
+     * @return  ExtArray 返回一个包含原数组所有值的新数组
+     */
+    public function values(): ExtArray
+    {
+        return new ExtArray(array_values($this->arr));
+    }
+
+    /**
+     * 判断给定的值``$value``是否在数组中
+     * @param mixed $value 要判断的值
+     * @return bool  如果``$value``在数组中则返回``true``,否则返回``false``
+     */
+    public function contains(mixed $value): bool
+    {
+        return in_array($value, $this->arr);
+    }
+
+    /**
+     * 判断给定的键``$key``是否在数组中
+     * @param mixed $key 要判断的键
+     * @return bool  如果``$key``在数组中则返回``true``,否则返回``false``
+     */
+    public function containsKey(mixed $key): bool
+    {
+        return array_key_exists($key, $this->arr);
+    }
 
 }
