8b487fa47bef67b62a6a177659d7ebdd9029d240
cruldra <cruldra@gmail.com>
2023-08-29 17:18:20 +0800

    1

---
+++ src/ExtString.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ExtensionsPhp;
+
+class ExtString
+{
+    private string $str;
+
+    /**
+     * ExtString constructor.
+     * @param $str
+     */
+    public function __construct(string $str)
+    {
+        $this->str = $str;
+    }
+
+
+    /**
+     * 替换字符串
+     *
+     * ```php
+     *
+     * $str = new ExtString('hello world');
+     * $str.replace('world','php'); // hello php
+     * $str.replace(['hello','world'],['hi','php']); // hi php
+     * $str.replace(['hello','world'],'nihao'); // nihao nihao
+     * ```
+     *
+     * @param array|string $search 要替换的字符串,如果是一个数组的话,则会替换所有的
+     * @param string $replace   要替换成的字符串
+     * @return static   返回当前对象
+     */
+    public function replace(array|string $search,string $replace): static
+    {
+        $this->str = str_replace($search, $replace, $this->str);
+        return $this;
+    }
+
+}
